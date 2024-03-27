b2ee48525bdc70034e4e215053feea8d76042237
moveuk <v_donguk@naver.com>
2024-03-27 17:06:14 +0900

    remove: 사용하지 않는 file 정리

--- src/Zziririt.js
+++
@@ -1,8 +0,0 @@
-import './App.css';
-import OldBoard from './components/OldBoard';
-
-export default function Zziririt({ data }) {
-  return (
-    <OldBoard data={data} /> //{/* data를 Main에 props로 전달 */}
-  );
-}
